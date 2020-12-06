540efc5601977b35301a92227d14e0374f857b9b
Farid Hidayat <e.faridhidayat@gmail.com>
2020-12-06 23:46:53 +0700

    Mobx with Providers

--- lib/screens/counter/index.dart
+++ lib/screens/counter/index.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
 import 'package:playingNumber/store/counter/counter.dart';
 
 class CounterApp extends StatefulWidget {
@@ -11,40 +12,44 @@ class CounterApp extends StatefulWidget {
 }
 
 class _CounterAppState extends State<CounterApp> {
-  Counter counter = Counter();
+  // Counter counter = Counter();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Observer(
-            builder: (_) => Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Flexible(
-                        flex: 1,
-                        child: Text(
-                          counter.value.toString(),
-                          style: Theme.of(context).textTheme.headline1,
-                        ),
+    return MultiProvider(
+        providers: [Provider<Counter>(create: (_) => Counter())],
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text(widget.title),
+            ),
+            body: Observer(builder: (context) {
+              final counter = context.watch<Counter>();
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Flexible(
+                      flex: 1,
+                      child: Text(
+                        counter.value.toString(),
+                        style: Theme.of(context).textTheme.headline1,
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FlatButton(
-                            onPressed: counter.decrement,
-                            child: Text("Minus"),
-                          ),
-                          FlatButton(
-                            onPressed: counter.increment,
-                            child: Text("Plus"),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                )));
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FlatButton(
+                          onPressed: counter.decrement,
+                          child: Text("Minus"),
+                        ),
+                        FlatButton(
+                          onPressed: counter.increment,
+                          child: Text("Plus"),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            })));
   }
 }
